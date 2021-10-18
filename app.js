da32519a206bc77845e011ccd0ccecc6ef9524d8
j-nathanson <jacobnath93@gmail.com>
2021-10-18 18:51:21 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const structureA = [
+  "Champ,",
+  "Fact:",
+  "Everybody says",
+  "Dang..",
+  "Check it:",
+  "Just saying...",
+  "Superstar,",
+  "Tiger,",
+  "Self,",
+  "Know this:",
+  "News alert:",
+  "Girl,",
+  "Ace",
+  "Excuse me but",
+  "Experts agree",
+  "In my opinion",
+  "Hear ye, hear ye:",
+  "Okay, Listen up:",
+];
+
+const structureB = [
+    'the mere idea of you',
+    'your soul',
+    'your hair today',
+    'everything you do',
+    'your personal style',
+    'every thought you have',
+    'that sparkle in your eye,'
+]
